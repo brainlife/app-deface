c0d139f1bfc6a6546e2c65b1ac7cb16ee579a568
Soichi Hayashi <hayashis@iu.edu>
2017-09-23 22:30:24 -0400

    added run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#for debugging
+[ ! $SERVICE_DIR ] && export SERVICE_DIR=`pwd`
+
+module load freesurfer
+
+time mri_deface `$SERVICE_DIR/jq -r '.t1' config.json` \
+	$FREESURFER_HOME/average/talairach_mixed_with_skull.gca \
+	$FREESURFER_HOME/average/face.gca \
+	t1.nii.gz
